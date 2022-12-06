525a55faaa9fdafb8c904dfc42b0a9686d5b4f46
tomrogers02 <108933448+tomrogers02@users.noreply.github.com>
2022-12-06 16:43:50 +0000

    Add files via upload
    Some small adjustments to mobile version for marketplace and home page. Testing if nav bar and wallet menu pop up on all pages.

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-
 //Loading screen?
 //Mobile carousel
 
 
 
 const countdown = () => {
-    const countDate= new Date ('December 30, 2022 00:00:00');
-    const now= new Date().getTime();
-    const gap= countDate-now;
+    const countDate = new Date('December 30, 2022 00:00:00');
+    const now = new Date().getTime();
+    const gap = countDate - now;
 
 
 
@@ -15,99 +14,99 @@ const countdown = () => {
     const minute = second * 60;
     const hour = minute * 60;
 
-    const textHour = Math.floor(gap/hour);
-    const textMinute = Math.floor((gap % hour)/minute);
-    const textSecond = Math.floor((gap % minute)/second);
-    console.log(textHour,textMinute,textSecond);
+    const textHour = Math.floor(gap / hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+    console.log(textHour, textMinute, textSecond);
 
 
     document.querySelector('.hour').innerText = textHour;
     document.querySelector('.minute').innerText = textMinute;
     document.querySelector('.second').innerText = textSecond;
-    
+
 
 
 };
 
-setInterval(countdown,1000)
+setInterval(countdown, 1000)
 
-const primaryNav =document.getElementById('desktop-nav');
-const navToggle =document.getElementById('mobile-cta');
-const mobileExit =document.getElementById('mobile-exit');
+const primaryNav = document.getElementById('desktop-nav');
+const navToggle = document.getElementById('mobile-cta');
+const mobileExit = document.getElementById('mobile-exit');
 
-navToggle.addEventListener('click',() => {
+navToggle.addEventListener('click', () => {
     const visibility = primaryNav.getAttribute('data-visible')
 
     console.log(visibility);
 
     if (visibility === "false") {
-        primaryNav.setAttribute("data-visible",true);
+        primaryNav.setAttribute("data-visible", true);
     }
 
 });
 
-mobileExit.addEventListener('click',() => {
+mobileExit.addEventListener('click', () => {
     const visibility = primaryNav.getAttribute("data-visible")
 
     console.log(visibility)
 
     if (visibility === "true") {
-        primaryNav.setAttribute("data-visible",false)
+        primaryNav.setAttribute("data-visible", false)
     }
 
 });
 
 
-const walletNav =document.getElementById('wallet-desktop-menu');
-const walletToggle =document.getElementById('wallet');
-const walletExit=document.getElementById('wallet-exit-btn');
+const walletNav = document.getElementById('wallet-desktop-menu');
+const walletToggle = document.getElementById('wallet');
+const walletExit = document.getElementById('wallet-exit-btn');
 
-walletToggle.addEventListener('click',() => {
+walletToggle.addEventListener('click', () => {
     const visibility = walletNav.getAttribute('data-visible')
 
     console.log(visibility);
 
     if (visibility === "false") {
-        walletNav.setAttribute("data-visible",true);
+        walletNav.setAttribute("data-visible", true);
     }
 
 });
 
-walletExit.addEventListener('click',() => {
+walletExit.addEventListener('click', () => {
     const visibility = walletNav.getAttribute("data-visible")
 
     console.log(visibility)
-    
+
 
     if (visibility === "true") {
-        walletNav.setAttribute("data-visible",false)
+        walletNav.setAttribute("data-visible", false)
     }
 
 });
 
 
-const filterPopUp =document.getElementById('filter-pop-up-menu');
-const filterToggle =document.getElementById('filter-dropdown')
-const filterExit =document.getElementById('filter-mobile-exit-btn')
+const filterPopUp = document.getElementById('filter-pop-up-menu');
+const filterToggle = document.getElementById('filter-dropdown')
+const filterExit = document.getElementById('filter-mobile-exit-btn')
 
-filterToggle.addEventListener('click',()=> {
+filterToggle.addEventListener('click', () => {
     const visibility = filterPopUp.getAttribute('data-visible')
 
     console.log(visibility);
 
     if (visibility === "false") {
-        filterPopUp.setAttribute("data-visible",true);
+        filterPopUp.setAttribute("data-visible", true);
     }
 
 });
 
-filterExit.addEventListener('click',() => {
-    const visibility=filterPopUp.getAttribute('data-visible')
+filterExit.addEventListener('click', () => {
+    const visibility = filterPopUp.getAttribute('data-visible')
 
     console.log(visibility)
 
     if (visibility === "true") {
-        filterPopUp.setAttribute("data-visible",false);
+        filterPopUp.setAttribute("data-visible", false);
     }
 
 });
@@ -116,15 +115,10 @@ filterExit.addEventListener('click',() => {
 
 window.addEventListener('load', () => {
     let scrollElement = document.querySelector('.media-scroller');
-    scrollElement.scrollLeft =  (scrollElement.scrollWidth - scrollElement.clientWidth ) / 2;
-  });
+    scrollElement.scrollLeft = (scrollElement.scrollWidth - scrollElement.clientWidth) / 2;
+});
 
-  window.addEventListener('load', () => {
+window.addEventListener('load', () => {
     let scrollElement = document.querySelector('.nft-carousel-two');
-    scrollElement.scrollLeft =  (scrollElement.scrollWidth - scrollElement.clientWidth ) / 2;
-  });
-
-  
-
-
-
+    scrollElement.scrollLeft = (scrollElement.scrollWidth - scrollElement.clientWidth) / 2;
+});
